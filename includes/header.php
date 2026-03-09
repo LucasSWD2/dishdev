10d75332b968056bf1d20892205b2877078c3d64
Lucas <6001992@mborijnland.nl>
2026-03-09 13:33:59 +0100

    test 2

---
+++ includes/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <header>
+        <section>
+            <img src="../images/logo_kleur_vlam_en_vlees.png" alt="Logo Vlam en Vlees">
+        </section>
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Reservatie</a>
+            <a href="#">Menu</a>
+            <a href="#">Vacatures</a>
+            <a href="#">Openingstijden</a>
+        </nav>
+    </header>
+</body>
+</html>
