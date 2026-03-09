6a60647d5669e96ff9d663d5ec4a66c65fcc2a6f
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-09 14:02:04 +0100

    header

--- includes/header.php
+++ includes/header.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/dishdev.css">
+    <link rel="stylesheet" href="../css/dishdev.css">
     <title>Document</title>
 </head>
 <body>
     <header class="Header">
-            <img scr="images/logo_vlam_en_vlees.png" alt="Logo" class="Logo"> 
+            <img scr="../images/logo_vlam_en_vlees.png" alt="Logo" class="Logo"> 
     </header>
 </body>
 </html>
