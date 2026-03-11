31abc59b4375780308e3203b2647c868744562e8
Lucas <6001992@mborijnland.nl>
2026-03-11 12:48:38 +0100

    footer klaar

--- includes/header.php
+++ includes/header.php
@@ -1,16 +1,12 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="nl">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/dishdev.css">
-    <title>Document</title>
-</head>
-
-<body>
+    <link rel="stylesheet" href="css/dishdev.css">
     <header class="Header">
         <section>
-            <img scr="../images/logo_vlam_en_vlees.png" alt="Logo" class="Logo"> 
-            <img scr="../images/Drie horizontale witte lijnen.png" alt="Lijn" class="Lijn"> 
+            <img src="images/logo_vlam_en_vlees.png" alt="Logo" class="Logo"> 
+            <img src="images/Drie horizontale witte lijnen.png" alt="Lijn" class="Lijn"> 
         </section>
     </header>
--- index.php
+++ index.php
@@ -1,12 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" src="css/dishdev.css">
+</head>
 <?php
-include "include/header.php";
+include "includes/header.php";
 ?>
+<body>
+    <section>
+        <section>
+            <img>
+            </section>
+            <article>
+                <h1>Reservatie</p>
+            </article>
+            <section>
+                <button>GO</button>
+        </section>
+        <section>
+            <img>
+            </section>
+            <article>
+                <h1>Menu</p>
+            </article>
+            <section>
+                <button>GO</button>
+        </section>
+        <section>
+            <img>
+            </section>
+            <article>
+                <h1>Openingstijden</p>
+            </article>
+            <section>
+                <button>GO</button>
+        </section>
+        <section>
+            <img>
+            </section>
+            <article>
+                <h1>Vacatures</p>
+            </article>
+            <section>
+                <button>GO</button>
+        </section>
+    </section>
+        
 
-
-<footer>
+</body>
 <?php
 include "includes/footer.php";
 ?>
-</footer>
-</body>
 </html>
