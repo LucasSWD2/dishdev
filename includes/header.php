770d678d0f0987a523649fa382e2a13a42db3060
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-16 13:06:54 +0100

    menu
    menu

--- includes/header.php
+++ includes/header.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/dishdev.css">
+    <link rel="stylesheet" href="css/dishdev.css">
     <title>Document</title>
 </head>
 
 <body>
     <header>
-        <section id="Headerlogos">
+        <section id="headerlogos">
             <img src="images/logo_vlam_en_vlees.png" alt="Logo" class="Logo"> 
             <img src="images/Drie_horizontale_witte_lijnen-removebg-preview.png" alt="Lijn" class="Lijn"> 
         </section>
