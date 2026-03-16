5b86550c708f531fb4245d29da9c167e792c6e64
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-16 14:31:56 +0100

    menu pagina klaar

--- menu.php
+++ menu.php
@@ -9,29 +9,50 @@
 <?php
 include "includes/header.php";
 ?>
+
 <body>
-<section>
+
+<section id="bodykleur">
+
+
 <img src= "images/menu foto.png" class="menuimg" alt= "menuimg">
+
+
+
+
 <section>
-
-
-
-<h1>
- STEAK
- 
- ZALM
-
- BURGER
-
- LUXE STEAK
+<h1 id="menunamen">
+ <strong>STEAK</strong> <strong>100€</strong> <br>
+ <p>Malse, sappige steak met een perfect gegrilde korst en volle, pure vleessmaak.</p>
+ <strong>ZALM</strong> <strong>100€</strong> <br>
+ <p>Zacht gegaarde zalm, sappig en verfijnd met een frisse, pure smaak. </p>
+ <strong>BURGER</strong> <strong>150€</strong><br>
+ <p>Sappige burger met een rijke grillsmaak en een perfect geroosterd broodje. </p>
+ <strong>LUXE STEAK</strong> <strong>150€</strong> <br>
+ <p>Premium steak, boterzacht en intens van smaak, perfect gegrild voor ultieme beleving. </p>
 </h1>
+</section> 
 
 
-  
-        
+
+<img src="images/Schermafbeelding 2026-03-16 140005.png" class="menuimg" alt="menuimg">
+
+<section>
+<h1 id="menunamen2">
+<strong>LUXE ZALM</strong> <strong>100€</strong>
+<p>Selecte zalmfilet, zijdezacht en elegant van smaak, verfijnd bereid.</p>
+</h1>
+</section>
+
+<img src="images/Schermafbeelding 2026-03-16 140448.png" class="menuimg" alt="menuimg">
+
+
+</section>
 
 </body>
+
 <?php
 include "includes/footer.php";
 ?>
 </html>
+
