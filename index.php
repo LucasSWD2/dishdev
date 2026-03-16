cdfa2e4dc67fe6c391ab947115d039830da44ed3
Lucas <6001992@mborijnland.nl>
2026-03-16 14:01:53 +0100

    home pagina desktop klaar

--- index.php
+++ index.php
@@ -12,41 +12,54 @@ include "includes/header.php";
 <body>
     <section class="gridCont">
         <section id="naviImg1">
-            <img src="images/Reservatie foto.png" alt="foto reservatie">
+           <a href="#"><img src="images/Reservatie foto.png" alt="foto reservatie"></a>
             </section>
             <article id="naviTekst1">
-                <h1>Reservatie</p>
+                <h1>Reservatie</h1>
             </article>
             <section id="naviButton1">
                 <button>GO</button>
         </section>
+        <article id="desktopHomeText1">
+            <a class="homeTextLink" href="#">Wilt u zeker zijn van een gezellige lunch of een sfeervol diner? Reserveer eenvoudig uw tafel via het formulier hieronder. Of u nu met z’n tweeën komt of een speciale gelegenheid viert, wij zorgen voor een warme ontvangst en heerlijke gerechten.
+                    Heeft u speciale wensen of dieetvoorkeuren? Laat het ons gerust weten bij uw reservatie.
+                    Wij kijken ernaar uit u binnenkort te verwelkomen.</a>
+                 </article>
         <section id="naviImg2">
-            <img src="images/menu foto.png" alt="foto menu">
+            <a href="#"><img src="images/menu foto.png" alt="foto menu"></a>
             </section>
             <article id="naviTekst2">
-                <h1>Menu</p>
+                <h1>Menu</h1>
             </article>
             <section id="naviButton2">
                 <button>GO</button>
         </section>
+        <article id="desktopHomeText2">
+             <a class="homeTextLink" href="#"> Ontdek onze gerechten, bereid met verse en seizoensgebonden ingrediënten. Van verfijnde klassiekers tot verrassende specialiteiten: er is voor ieder wat wils. Laat u inspireren en geniet van pure smaken en passie op het bord.</a> </article>
         <section id="naviImg3">
-            <img src="images/Openingstijden foto.png" alt="foto openingstijden">
+            <a href="#"><img src="images/Openingstijden foto.png" alt="foto openingstijden"></a>
             </section>
             <article id="naviTekst3">
-                <h1>Openingstijden</p>
+                <h1>Openingstijden</h1>
             </article>
             <section id="naviButton3">
                 <button>GO</button>
         </section>
+        <article id="desktopHomeText3"> 
+            <a class="homeTextLink" href="#"> Wij heten u van harte welkom voor lunch en diner op onderstaande dagen en tijden. Bekijk hier onze actuele openingstijden en plan uw bezoek op het moment dat het u het beste uitkomt.</a>
+        </article>
         <section id="naviImg4">
-            <img src="images/vacatures foto.png" alt="foto vacatures">
+            <a href="#"><img src="images/vacatures foto.png" alt="foto vacatures"></a>
             </section>
             <article id="naviTekst4">
-                <h1>Vacatures</p>
+                <h1>Vacatures</h1>
             </article>
             <section id="naviButton4">
                 <button>GO</button>
         </section>
+        <article id="desktopHomeText4"> 
+            <a class="homeTextLink" href="#">Wilt u deel uitmaken van ons enthousiaste team? Wij zijn altijd op zoek naar gemotiveerde collega’s met een passie voor horeca en gastvrijheid. Bekijk hier onze openstaande vacatures en solliciteer vandaag nog. </a>
+        </article>
     </section>
         
 
