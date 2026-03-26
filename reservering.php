00d532a28d86d93f030ce3d78a9c296e5a66fc0d
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-26 23:35:22 +0100

    reservateren begin

--- reservering.php
+++ reservering.php
@@ -4,13 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="css/dishdev.css">
 </head>
 <body>
 <?php
 include "includes/header.php";
 ?>
 
+<section id="innerkleur">
 
+</section>
 
 
 
