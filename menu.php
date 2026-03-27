3b800f2c7b8c23a912b17a7f01489da453c9aa1e
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-27 21:29:20 +0100

    menu pagina verbetering

--- menu.php
+++ menu.php
@@ -17,19 +17,15 @@ include "includes/header.php";
 
 <section id="deskmenuimg">
 
-<h1>Menu</h1>
-
-
 
 <img src="images/Screenshot 2026-03-23 150056.png" class="deskmenuimg" alt="deskmenuimg">
 <img src="images/Schermafbeelding 2026-03-23 150501.png" class="deskmenuimg" alt="deskmenuimg">
 
-
-
-<img src="images/Schermafbeelding 2026-03-23 150507.png" class="deskmenuimg" alt="deskmenuimg">
-<img src="images/Schermafbeelding 2026-03-23 150437.png" class="deskmenuimg" alt="deskmenuimg">
-
 </section>
+
+<h1 id="menutitle">Menu</h1> 
+
+
 
 
 
@@ -59,60 +55,71 @@ include "includes/header.php";
 
 <img src="images/Schermafbeelding 2026-03-16 140005.png" class="menuimg" alt="menuimg">
 
-<section>
-<h1 id="menunamen2">
-<article>
-<strong>LUXE ZALM</strong> <strong>-100€</strong>
-<p>Selecte zalmfilet, zijdezacht en elegant van smaak, verfijnd bereid.</p>
-</h1>
-</article>
+<section class="menu">
+
+  <section class="menu1">
+    <span class="naam">STEAK</span>
+    <span class="prijs">€100</span>
+    <p>Malse, sappige steak met een perfect gegrilde korst en volle, pure vleessmaak.</p>
+  </section>
+
+  <section class="menu2">
+    <span class="naam">ZALM</span>
+    <span class="prijs">€100</span>
+    <p>Zacht gegaarde zalm, sappig en verfijnd met een frisse, pure smaak.</p>
+  </section>
+
+  <section class="menu3">
+    <span class="naam">Carpaccio van zalm</span>
+    <span class="prijs">€100</span>
+    <p>Dun gesneden zalm met citroenolie, kappertjes en frisse dille.</p>
+  </section>
+
+  <section class="menu4">
+    <span class="naam">Geroosterde paprikasoep</span>
+    <span class="prijs">€100</span>
+    <p>Romige soep van zoete paprika met een vleugje knoflook en basilicum.</p>
+  </section>
+
+  <section class="menu5">
+    <span class="naam">BURGER</span>
+    <span class="prijs">€150</span>
+    <p>Sappige burger met een grillsmaak en geroosterd broodje.</p>
+  </section>
+
+  <section class="menu6">
+    <span class="naam">LUXE STEAK</span>
+    <span class="prijs">€150</span>
+    <p>Premium steak, boterzacht en perfect gegrild voor ultieme beleving.</p>
+  </section>
+
+  <section class="menu7">
+    <span class="naam">Gegrilde entrecote</span>
+    <span class="prijs">€100</span>
+    <p>Rundvlees met kruidenboter, geserveerd met friet en salade.</p>
+  </section>
+
+  <section class="menu8">
+    <span class="naam">Pasta pesto kip</span>
+    <span class="prijs">€100</span>
+    <p>Pasta met kip, groene pesto, cherrytomaat en Parmezaanse kaas.</p>
+  </section>
+
+  <section class="menu9">
+    <span class="naam">LUXE ZALM</span>
+    <span class="prijs">€100</span>
+    <p>Selecte zalmfilet, zijdezacht en elegant van smaak, verfijnd bereid.</p>
+  </section>
+
 </section>
 
+<section id="deskmenuimg2"> 
 
-<img src="images/Schermafbeelding 2026-03-16 140448.png" class="menuimg" alt="menuimg">
-
-
-
-
-
-<section>
-    <h1 id ="deskmenunamen1">
-<article>
- <strong>STEAK</strong> <strong>-100€</strong> <br>
- <p>Malse, sappige steak met een perfect gegrilde korst en volle, pure vleessmaak.</p>
- <strong>ZALM</strong> <strong>-100€</strong> <br>
- <p>Zacht gegaarde zalm, sappig en verfijnd met een frisse, pure smaak. </p>
-  <strong>Carpaccio van zalm</strong> <strong>-100€</strong> <br>
- <p>Dun gesneden zalm met citroenolie, kappertjes en frisse dille.</p>
- <strong>Geroosterde paprikasoep</strong> <strong>-100€</strong> <br>
- <p>Romige soep van zoete paprika met een vleugje knoflook en basilicum. </p>
-</article>
+<img src="images/Schermafbeelding 2026-03-23 150507.png" class="deskmenuimg" alt="deskmenuimg2">
+<img src="images/Schermafbeelding 2026-03-23 150437.png" class="deskmenuimg" alt="deskmenuimg2">
 
 </section>
 
-<section>
-    <h1 id ="deskmenunamen2">
-<article>
- <strong>BURGER</strong> <strong>-150€</strong><br>
- <p>Sappige burger met een grillsmaak en geroosterd broodje. </p>
- <strong>LUXE STEAK</strong> <strong>-150€</strong> <br>
- <p>Premium steak, boterzacht en perfect gegrild voor ultieme beleving. </p>
-  <strong>Gegrilde entrecote</strong> <strong>-100€</strong> <br>
- <p>rundvlees met kruidenboter, geserveerd met friet en salade.</p>
- <strong>Pasta pesto kip</strong> <strong>-100€</strong> <br>
- <p>pasta met kip, groene pesto, cherrytomaat en Parmezaanse kaas.</p>
-</article>
-
-</section>
-
-<section>
-<h1 id ="deskmenunamen3">
-<article>
-<strong>LUXE ZALM</strong> <strong>-100€</strong>
-<p>Selecte zalmfilet, zijdezacht en elegant van smaak, verfijnd bereid.</p>
-</article>
-
-</section>
 
 </section>
 
