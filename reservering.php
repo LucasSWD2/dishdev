2e4a28d432ec0d3bb52c641db4da62d338299a86
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-27 19:47:19 +0100

    balkjes

--- reservering.php
+++ reservering.php
@@ -6,35 +6,45 @@
     <title>Document</title>
     <link rel="stylesheet" href="css/dishdev.css">
 </head>
-<body>
 <?php
 include "includes/header.php";
 ?>
-
 <section id="innerkleur">
 
+<body>
+
+
+
+
+</section>
+
+<section id="reservetext">
+    <articel>
+    <h1>Naam</h1>
+    <h1>telefoon</h1>
+    <h1>Email</h1>
+    <h1>Aantal personen</h1>
+    <h1>datum</h1>
+    <h1>Tijd</h1>
+    <h1>Eventueele Opmerkingen</h1>
+    <articel>
+</section>
+
+
+<section id="reservebalk">
+<input type="text" placeholder="...">
+<input type="text" placeholder="...">
+<input type="text" placeholder="...">
+<input type="text" placeholder="...">
 </section>
 
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+</body>
+</section>
 <?php
 include "includes/footer.php";
 ?>
-</body>
 </html>
