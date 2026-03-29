40086330e7a2b5f71c77fa0925c77b1a4f66cbec
Lucas <6001992@mborijnland.nl>
2026-03-29 20:47:46 +0200

    mobile first
    vacature pagina mobile first gemaakt en mobile versie klaar

--- includes/footer.php
+++ includes/footer.php
@@ -2,20 +2,19 @@
  <footer>
     <section id="footerLogoEnText">
         <section>
-            <h1 class="footerText">Vlam & Vlees</h1>
-            <p class="footerText">
-                Promenade 12 <br>
-                2711 AB zoetemeer <br>
-                Email:info@vlamenvleeszoetermeer.nl<br>
-                Telefoonnummer:088-1321111
-            </p>
+            <article>
+                <p>Promenade 12</p>
+                <p>2711 AB Zoetermeer</p>
+                <p>email:info@vlamenvleeszoetermeer.nl</p>
+                <p>telefoonnummer:088-1321111</p>
+            </article>
         </section>
         <section>
-            <img src="images/footer image.png" alt="Foto Lunchroom">
+            <img src="images/footer image.png" alt="restaurant foto">
         </section>
     </section>
-    <section>
-        <p id="namen">Lucas & Kirubel</p>
-</section>
-    </footer>
+    <section id="namen">
+        <p>Lucas & Kirubel</p>
+    </section>
+</footer>
 
