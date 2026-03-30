ca40db16eed320afb014c49d9786d47488519188
Kirubel12345678910 <6053472@mborijnland.nl>
2026-03-30 13:08:25 +0200

    fixed

--- includes/header.php
+++ includes/header.php
@@ -17,7 +17,7 @@
   <section id="Headernav">
     <nav id="navMenu">
       <a href="index.php">Home</a>
-      <a href="Reservatie.php">Reservatie</a>
+      <a href="reservering.php">Reservatie</a>
       <a href="Menu.php">Menu</a>
       <a href="Vacatures.php">Vacatures</a>
       <a href="Openingstijden.php">Openingstijden</a>
